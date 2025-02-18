4022aeed00855eff3f4660fce9764e0e15a9f2fa
norwalneto <norwalgomess@gmail.com>
2025-02-17 22:15:06 -0300

    arquivos iniciais.

---
+++ board-de-tarefas/src/main/java/com/projeto/board_de_tarefas/BoardDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.board_de_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardDeTarefasApplication.class, args);
+	}
+
+}
